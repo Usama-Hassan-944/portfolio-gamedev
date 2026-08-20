b0e59a46dc75998e32ff17e5949b2eeb10a332c2
Usama Hassan <usamahassan944@gmail.com>
2026-08-20 15:52:09 +0500

    changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/usama-hassan-944/portfolio'
+      href='https://github.com/usama-hassan-944/portfolio-gamedev'
       className='link footer__link'
     >
       Created By Usama Hassan
